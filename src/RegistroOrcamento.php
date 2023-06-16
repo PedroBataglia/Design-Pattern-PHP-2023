d42a2b4a933822784275edc49bc640626ad702b5
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-06-16 09:36:16 -0300

    implmentando metodo adapter

---
+++ src/RegistroOrcamento.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+use Alura\DesignPattern\EstadosOrcamentos\Finalizado;
+use Alura\DesignPattern\Http\HttpAdapter;
+
+class RegistroOrcamento
+{
+    private HttpAdapter $http;
+
+    public function __construct(HttpAdapter $http)
+    {
+        $this->http = $http;
+    }
+
+    public function registrar(Orcamento $orcamento): void
+    {
+        if ($orcamento->estadoAtual instanceof Finalizado) {
+            throw new \DomainException('Apenas orcamentos finalizados podem ser registrados na api');
+        }
+
+        $this->http->post('http://api.registrar.orcamento', [
+            'valor' => $orcamento->valor,
+            'quantidadeItens' => $orcamento->quantidadeItens,
+        ]);
+    }
+}
---
+++ src/registro-orcamento.php
@@ -0,0 +1,10 @@
+<?php
+
+use Alura\DesignPattern\Http\ReactPHPHttpAdapter;
+use Alura\DesignPattern\Orcamento;
+use Alura\DesignPattern\RegistroOrcamento;
+
+require_once 'vendor/autoload.php';
+
+$registroOrcamento = new RegistroOrcamento(new ReactPHPHttpAdapter());
+$registroOrcamento->registrar(new Orcamento());
